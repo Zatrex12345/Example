4f9c6a4b86a5d38a975b7281e5524a831090aed9
Zatrex <135520176+ZatrexReal@users.noreply.github.com>
2024-06-09 10:37:26 +0100

    removve

--- src/main/java/me/zatrex/exampleplugin/listeners/PlayerJoinListener.java
+++ src/main/java/me/zatrex/exampleplugin/listeners/PlayerJoinListener.java
@@ -5,7 +5,6 @@ import me.zatrex.exampleplugin.utils.CC;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener  {
