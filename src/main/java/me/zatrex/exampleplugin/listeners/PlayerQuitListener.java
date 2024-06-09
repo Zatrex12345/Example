a9575807fd9ea6e6ba56dae89e4db92354dd0de9
Zatrex <135520176+ZatrexReal@users.noreply.github.com>
2024-06-09 10:40:42 +0100

    Quit Listener

--- src/main/java/me/zatrex/exampleplugin/listeners/PlayerQuitListener.java
+++ src/main/java/me/zatrex/exampleplugin/listeners/PlayerQuitListener.java
@@ -1,6 +1,7 @@
 package me.zatrex.exampleplugin.listeners;
 
 import me.zatrex.exampleplugin.utils.CC;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
